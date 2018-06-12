e16bf0483e9b0cefb11712087880b8c9291b89ff
Corwin Perren <caperren@gmail.com>
2018-06-11 20:23:06 -0700

    i3 and monitor configuration scripts

---
+++ apply_monitor_config.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+xrandr --output DVI-D-0 --left-of DVI-I-1 && xrandr --output HDMI-0 --left-of DVI-D-0 && xrandr --output DVI-D-0 --left-of DVI-I-1 && xrandr --output HDMI-0 --left-of DVI-D-0
+xsetroot -solid "#000000"
